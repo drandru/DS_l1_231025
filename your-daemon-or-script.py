2a4ed965af83ec549b0d4b490e52cd6648f5528c
Smirnov Andrew <a.smirnov@gorodtroika.ru>
2023-10-28 15:32:48 +0300

    init work

--- your-daemon-or-script.py
+++ your-daemon-or-script.py
@@ -1,7 +1,17 @@
 
 
 
-print( (5 * 5.6 * (24-13)) ** 0.5)
+#print( (5 * 5.6 * (24-13)) ** 0.5)
 
 
+#number = float(input('Enter your number: '))
+
+
+#print(int((0.7 + 0.1) * 10));
+#print(type(number));
+
+
+gradient_boosting = "Gradient boosting"
+
+print(gradient_boosting[-5:]);
 
